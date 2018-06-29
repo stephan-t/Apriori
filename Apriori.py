a2479917d702ab4624dba0a1616794ddb3d83d19
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-06-28 22:11:57 -0400

    Add initial pass function

--- Apriori.py
+++ Apriori.py
@@ -1,11 +1,32 @@
 import pandas as pd
 
 # Create transaction set
-T = pd.DataFrame({"Items":
-                 [{"Beef", "Chicken", "Milk"},
-                 {"Beef", "Cheese"},
-                 {"Cheese", "Boots"},
-                 {"Beef", "Chicken", "Cheese"},
-                 {"Beef", "Chicken", "Clothes", "Cheese", "Milk"},
-                 {"Chicken", "Clothes", "Milk"},
-                 {"Chicken", "Milk", "Clothes"}]})
+T = pd.DataFrame({"items":
+                 [{"beef", "chicken", "milk"},
+                 {"beef", "cheese"},
+                 {"cheese", "boots"},
+                 {"beef", "chicken", "cheese"},
+                 {"beef", "chicken", "clothes", "cheese", "milk"},
+                 {"chicken", "clothes", "milk"},
+                 {"chicken", "milk", "clothes"}]})
+
+# Minimum support
+min_sup = .30
+
+# Initial pass function
+def initpass(T):
+    C1 = pd.DataFrame(columns=['items', 'count'])
+
+    # Count support of individual items
+    for i in range(len(T)):
+        for j in T['items'][i]:
+            if j in list(C1['items']):
+                C1['count'][list(C1['items']).index(j)] += 1
+            else:
+                C1 = C1.append(pd.Series([j, 1], index=['items', 'count']), ignore_index=True)
+
+    C1 = C1.sort_values('items') # Sort items lexicographically
+    return C1
+
+# First pass over T
+C1 = initpass(T)
