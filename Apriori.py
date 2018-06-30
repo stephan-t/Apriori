03ac17b6de2a4716b188fdd7e8ae41cbefa0599a
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-06-30 16:31:01 -0400

    Start candidate generation pruning step

--- Apriori.py
+++ Apriori.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from itertools import combinations
 
 # Create transaction set
 T = pd.DataFrame({"items":
@@ -36,7 +37,7 @@ C1 = initpass(T)  # First pass over T
 F1 = C1[C1['count']/len(T) >= min_sup]  # Determine frequent itemsets
 F1 = F1.reset_index(drop=True)
 
-# Candidate generation
+# F2 Candidate generation
 C2 = pd.DataFrame(columns=['items', 'count'])
 
 for i in range(len(F1)):
@@ -44,8 +45,6 @@ for i in range(len(F1)):
         # Join two itemsets and add to candidates
         C2 = C2.append(pd.Series([[F1['items'][i], F1['items'][j]], 0],
                                  index=['items', 'count']), ignore_index=True)
-
-        # Delete candidate if a subset is not in k-1 frequent itemset       TODO
 
 # Check if each candidate itemset is in transaction set
 for i in range(len(T)):
@@ -57,3 +56,19 @@ for i in range(len(T)):
 # Determine frequent itemsets
 F2 = C2[C2['count']/len(T) >= min_sup]
 F2 = F2.reset_index(drop=True)
+
+
+# F3 Candidate generation
+C3 = pd.DataFrame(columns=['items', 'count'])
+
+for i in range(len(F2)):
+    for j in range(i+1, len(F2)):
+        # Find pairs that differ only by last item
+        if (F2['items'][i][:-1] == F2['items'][j][:-1]) & (F2['items'][i][-1] != F2['items'][j][-1]):
+            # Join two itemsets and add to candidates
+            C3 = C3.append(pd.Series([F2['items'][i] + [F2['items'][j][-1]], 0],
+                                     index=['items', 'count']), ignore_index=True)
+
+            # Delete candidate if a subset is not in k-1 frequent itemset
+            for k in range(len(C3)):
+                s = [list(i) for i in combinations(C3['items'][k], 2)]
