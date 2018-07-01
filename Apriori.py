4d04b24ad9ed1e97a605a67508db4b4aa8d46528
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-07-01 18:19:17 -0400

    Change candidate/frequent itemset generation to functions
    Variable names for candidate and frequent itemsets are created
    dynamically using a dictionary data structure.

--- Apriori.py
+++ Apriori.py
@@ -1,87 +1,84 @@
 import pandas as pd
 from itertools import combinations
 
-# Create transaction set
+# Create transaction dataset
 T = pd.DataFrame({"items":
-                 [("beef", "chicken", "milk"),
-                  ("beef", "cheese"),
-                  ("cheese", "boots"),
-                  ("beef", "chicken", "cheese"),
-                  ("beef", "chicken", "clothes", "cheese", "milk"),
-                  ("chicken", "clothes", "milk"),
-                  ("chicken", "milk", "clothes")]})
+                  [("beef", "chicken", "milk"),
+                   ("beef", "cheese"),
+                   ("cheese", "boots"),
+                   ("beef", "chicken", "cheese"),
+                   ("beef", "chicken", "clothes", "cheese", "milk"),
+                   ("chicken", "clothes", "milk"),
+                   ("chicken", "milk", "clothes")]})
 
 # Minimum support
 min_sup = .30
 
 
 # Initial pass function
-def initpass(T):
-    C1 = pd.DataFrame(columns=['items', 'count'])
+def init_pass(T):
+    global C
+    C = {1: pd.DataFrame(columns=['items', 'count'])}
 
     # Count support of individual items
     for i in range(len(T)):
-        for j in T['items'][i]:
-            if j in tuple(C1['items']):
-                C1['count'][tuple(C1['items']).index(j)] += 1
+        for j in range(len(T['items'][i])):
+            if {(T['items'][i][j],)}.issubset(tuple(C[1]['items'])):
+                C[1]['count'][tuple(C[1]['items']).index(((T['items'][i][j]),))] += 1
             else:
-                C1 = C1.append(pd.Series([j, 1], index=['items', 'count']), ignore_index=True)
+                C[1] = C[1].append(pd.Series([(T['items'][i][j],), 1],
+                                             index=['items', 'count']), ignore_index=True)
 
-    C1 = C1.sort_values('items')  # Sort items lexicographically
-    C1 = C1.reset_index(drop=True)
-    return C1
+    C[1] = C[1].sort_values('items')  # Sort items lexicographically
+    C[1] = C[1].reset_index(drop=True)
+    return C
+
+
+# Candidate generation function
+def candidate_gen(k):
+    C[k] = pd.DataFrame(columns=['items', 'count'])
+
+    for i in range(len(F[k - 1])):
+        for j in range(i + 1, len(F[k - 1])):
+            # Find pairs that differ only by last item
+            if (F[k - 1]['items'][i][:-1] == F[k - 1]['items'][j][:-1]) & \
+                    (F[k - 1]['items'][i][-1] != F[k - 1]['items'][j][-1]):
+                # Join two itemsets and add to candidates
+                C[k] = C[k].append(pd.Series([(F[k - 1]['items'][i]) + (F[k - 1]['items'][j][-1],), 0],
+                                             index=['items', 'count']), ignore_index=True)
+
+                # Delete candidate if a subset is not in k-1 frequent itemset
+                s = tuple(combinations(C[k]['items'][len(C[k]) - 1], len(C[k]['items'][0]) - 1))
+                if not set(s).issubset(tuple(F[k - 1]['items'])):
+                    C[k] = C[k].drop(C[k].index[-1])
+    return C
 
 
 # Apriori algorithm
-C1 = initpass(T)  # First pass over T
-F1 = C1[C1['count']/len(T) >= min_sup]  # Determine frequent itemsets
-F1 = F1.reset_index(drop=True)
+def apriori(T):
+    C = init_pass(T)  # First pass over T
+    global F
+    F = {1: C[1][C[1]['count'] / len(T) >= min_sup]}  # Determine frequent itemsets
+    F[1] = F[1].reset_index(drop=True)
 
-# F2 Candidate generation
-C2 = pd.DataFrame(columns=['items', 'count'])
+    # Subsequent passes over T
+    k = 2
+    while not F[k - 1].empty:
+        C = candidate_gen(k)
 
-for i in range(len(F1)):
-    for j in range(i+1, len(F1)):
-        # Join two itemsets and add to candidates
-        C2 = C2.append(pd.Series([(F1['items'][i], F1['items'][j]), 0],
-                                 index=['items', 'count']), ignore_index=True)
+        # Check if each candidate itemset is in transaction dataset
+        for i in range(len(T)):
+            for j in range(len(C[k])):
+                if set(C[k]['items'][j]).issubset(T['items'][i]):
+                    C[k]['count'][j] += 1
 
-# Check if each candidate itemset is in transaction set
-for i in range(len(T)):
-    for j in range(len(C2)):
-        if set(C2['items'][j]).issubset(T['items'][i]):
-            C2['count'][j] += 1
+        # Determine frequent itemsets
+        F[k] = C[k][C[k]['count'] / len(T) >= min_sup]
+        F[k] = F[k].reset_index(drop=True)
 
+        k += 1
 
-# Determine frequent itemsets
-F2 = C2[C2['count']/len(T) >= min_sup]
-F2 = F2.reset_index(drop=True)
-
-
-
-# F3 Candidate generation
-C3 = pd.DataFrame(columns=['items', 'count'])
-
-for i in range(len(F2)):
-    for j in range(i+1, len(F2)):
-        # Find pairs that differ only by last item
-        if (F2['items'][i][:-1] == F2['items'][j][:-1]) & (F2['items'][i][-1] != F2['items'][j][-1]):
-            # Join two itemsets and add to candidates
-            C3 = C3.append(pd.Series([F2['items'][i] + (F2['items'][j][-1],), 0],
-                                     index=['items', 'count']), ignore_index=True)
-
-            # Delete candidate if a subset is not in k-1 frequent itemset
-            s = tuple(combinations(C3['items'][len(C3)-1], 2))
-            if not set(s).issubset(tuple(F2['items'])):
-                C3 = C3.drop(C3.index[-1])
-
-# Check if each candidate itemset is in transaction set
-for i in range(len(T)):
-    for j in range(len(C3)):
-        if set(C3['items'][j]).issubset(T['items'][i]):
-            C3['count'][j] += 1
+    return F
 
 
-# Determine frequent itemsets
-F3 = C3[C3['count']/len(T) >= min_sup]
-F3 = F3.reset_index(drop=True)
+apriori(T)
