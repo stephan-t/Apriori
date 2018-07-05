dbca8f512c81e61aca8682e3babfe2ef6ddb9a9f
stephan-t <40370954+stephan-t@users.noreply.github.com>
2018-07-05 19:27:17 -0400

    Add output for rules with m-item consequents

--- rules.py
+++ rules.py
@@ -2,37 +2,48 @@ from itertools import combinations
 from apriori import candidate_gen
 import pandas as pd
 
+
 # Association rule generation algorithm
 def rule_gen(T, F, min_conf):
     for i in range(2, len(F)):  # Start from frequent 2-itemsets
         for j in range(len(F[i])):
-            con = tuple(combinations(F[i]['items'][j], 1))  # Association rule consequents (1-item subsets)
+            f = F[i].loc[j, ['items', 'count']]  # Frequent k-itemset (f[0] = itemset, f[1] = count)
+            con = tuple(combinations(f[0], 1))  # Generate 1-item consequents
+
             for c in con:
+                ant = tuple(sorted(set(f[0]) - set(c)))  # Association rule antecedents
+                ant_cnt = int(F[len(ant)]['count'][F[len(ant)]['items'] == ant])  # Antecedent support count
 
-                ant = tuple(set(F[i]['items'][j]) - set(c))  # Association rule antecedents
-                l = len(ant)  # Antecedent itemset size
-                ant = tuple(sorted(ant))
-                ant_cnt = int(F[l]['count'][F[l]['items'] == ant])  # Antecedent support count
-                f_cnt = F[i]['count'][j]  # Frequent itemset support count
+                if f[1] / ant_cnt >= min_conf:
+                    # Output association rules meeting minimum confidence
+                    print(str(ant) + " -> " + str(c) + " [sup = " + str(f[1]) + "/" + str(len(T)) +
+                          ", conf = " + str(f[1]) + "/" + str(ant_cnt) + "]")
 
-                # Output association rules meeting minimum confidence
-                if f_cnt/ant_cnt >= min_conf:
-                    print(str(ant) + " -> " + str(c) + " [sup = " + str(f_cnt) + "/" + str(len(T)) +
-                                                          ", conf = " + str(f_cnt) + "/" + str(ant_cnt) + "]")
-
-                    # Store 1-item consequents
-                    if not 'H' in locals():
+                    # Store 1-item consequents meeting minimum confidence
+                    if 'H' not in globals():
                         H = {1: pd.DataFrame(pd.Series((c,)), columns=['items'])}
                     else:
                         H[1] = H[1].append(pd.Series((c,), index=['items']), ignore_index=True)
 
-                    f = F[i].loc[j, ['items','count']]  # Frequent k-itemset
-                    ap_rule_gen(f, H, 1)  # Generate all rules from 1-item consequents
+                    rule_gen_all(T, F, f, H, 1, min_conf)  # Generate all rules from 1-item consequents
 
 
-# All rule generation procedure
-def all_rule_gen(f, H, m):  # H_m is set of m-item consequents
-    k = len(f)  # Frequent itemset size
-    if k > m + 1 and H[m] != ((),):
-        H = candidate_gen(H, m)
+# Generate all rules procedure
+def rule_gen_all(T, F, f, H, m, min_conf):
+    if (len(f[0]) > m + 1) and (not H[m].empty):
+        H = candidate_gen(H, m)  # Generate consquent supersets
 
+        for h in H[m + 1]['items']:
+            ant = tuple(set(f[0]) - set(h))  # Association rule antecedents
+            ant_cnt = int(F[len(ant)]['count'][F[len(ant)]['items'] == ant])  # Antecedent support count
+            conf = f[1] / ant_cnt  # Confidence of rule with (m + 1)-item consequents
+
+            if conf >= min_conf:
+                # Output association rules meeting minimum confidence
+                print(str(ant) + " -> " + str(h) + " [sup = " + str(f[1]) + "/" + str(len(T)) +
+                      ", conf = " + str(f[1]) + "/" + str(ant_cnt) + "]")
+            else:
+                # Delete consequents not meeting minimum confidence
+                H[m + 1] = H[m + 1].drop(H[m + 1].index[-1])
+
+        rule_gen_all(T, F, f, H, m + 1, min_conf)  # Generate all rules from (m + 1)-item consequents
